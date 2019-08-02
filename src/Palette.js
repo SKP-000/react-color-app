8da5cad943d234752f8a3fd513d1c9ae118d809b
SKP <skp.frl@gmail.com>
2019-08-02 21:27:37 +0530

    Created palette skeleton

---
+++ src/Palette.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Palette extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        {/* Navbar here. TODO */}
+        <div className="Palette-colors">
+          {/* a bunch of color boxes here. TODO */}
+        </div>
+        {/* Footer here... eventually. TODO */}
+      </div>
+    )
+  }
+}
+
+export default Palette;
