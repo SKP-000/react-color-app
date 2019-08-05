83b07efbaf36f4c0e3a5f955228c97f48a0689d8
SKP <skp.frl@gmail.com>
2019-08-05 20:07:37 +0530

    Added Select and Material UI

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import Select from '@material-ui/core/Select';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
@@ -33,6 +37,23 @@ export default class Navbar extends Component {
 
             />
         </div>
+
+        <div className="Navbar-selector">
+            <Select 
+              value={this.props.colorFormat}
+              onChange={this.props.handleChange}
+              name='color-format'
+              className='format-selector'
+          >
+            
+            <MenuItem value='hex'>HEX - #ffffff</MenuItem>
+            <MenuItem value='rgb'>RGB - rgb(255, 255, 255)</MenuItem>
+            <MenuItem value='rgba'>RGBA - rgba(255, 255, 255, 1.0)</MenuItem> 
+
+            </Select>
+        </div>
+
+
       </header>
     )
   }
--- src/Palette.js
+++ src/Palette.js
@@ -5,25 +5,54 @@ import './Palette.css';
 
 class Palette extends Component {
 
+  /** STATE
+   * - Level is set to 500 by default. It controls the lightness/darkness of the colors
+   * - The color format is initially set to hex {format: 'hex'} and is changed depending on what the user selects (i.e the color format) in the Navbar
+   */
+
   state = {
-    level: 500
+    level: 500,
+    format: 'hex',
+    colorFormat: 'HEX - #AB7C90'
   }
 
   changeLevel = (level) => {
     this.setState({ level });
   }
 
+  colorFormatHandler = () => {
+    if (this.state.format === 'hex') {
+      return 'HEX - #AB7C90'
+    } else if (this.state.format === 'rgb') {
+      return 'RGB - rgb(237, 222, 140)'
+    } else if (this.state.format === 'rgba') {
+      return 'RGBA - rgba(237, 222, 140, 0.8)'
+    }
+  }
+
+  // To be passed into Navbar and used in Select component in Navbar
+  changeFormat = evt => {
+    this.setState({ format: evt.target.value }, () => {
+      this.setState({ colorFormat: this.colorFormatHandler() });
+    });
+  }
+
   render() {
     const { colors } = this.props.palette;
     const { level } = this.state;
     const ColorBoxes = colors[level].map(colorDetails => (
-      <ColorBox background={colorDetails.hex} name={colorDetails.name} />
+      <ColorBox background={colorDetails[this.state.format]} name={colorDetails.name} />
     ));
 
     return (
       <div className="Palette">
         {/* Navbar here. TODO */}
-        <Navbar level={level} changeLevel={this.changeLevel} />
+        <Navbar
+          level={level}
+          changeLevel={this.changeLevel}
+          handleChange={this.changeFormat}
+          colorFormat={this.state.format}
+        />
         <div className="Palette-colors">
           {ColorBoxes}
           {/* a bunch of color boxes here. TODO */}
