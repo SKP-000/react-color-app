eeb341ea775d75ed5ac6427140d4af2116cc07d1
SKP <skp.frl@gmail.com>
2019-08-10 15:53:27 +0530

    Styling of PaletteList half done

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,21 +1,70 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import MiniPalette from './MiniPalette';
+import styled from 'styled-components';
 import './PaletteList.css';
+
+// TODO: Complete this Component I guess
+// FIXME: Nothing to fix (at least not yet). yay!
+
+const Root = styled.div`
+  background-color: blue;
+  height: 100%;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  flex-wrap: wrap;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  color: white;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+const Palettes = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 30%);
+  grid-gap: 5%;
+`;
+
+const PaletteLink = styled.a`
+  text-decoration: none;
+  color: inherit;
+`;
 
 class PaletteList extends Component {
 
   render() {
     const { palettes } = this.props;
     return (
-      <div className="PaletteList">
-        <h1 className="PaletteList-title">React Colors App</h1>
-        {palettes.map(palette => 
-          <Link className="PaletteLink" exact to={`/palettes/${palette.id}`}>
-            <MiniPalette {...palette} />
-          </Link>
-        )}
-      </div>
+      <Root>
+        <Container>
+          
+          <Nav>
+              <h1 className="title">React Colors App</h1>
+          </Nav>
+
+          <Palettes>
+            {palettes.map(palette => 
+              <PaletteLink as={Link} exact to={`/palette/${palette.id}`}>
+                <MiniPalette {...palette} />
+              </PaletteLink>
+            )}
+          </Palettes>
+
+        </Container>
+      </Root>
     )
   }
 }
