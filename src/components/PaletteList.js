cb1599cb36a469c976e90f527dbe14bf679a69d2
SKP <skp.frl@gmail.com>
2019-08-28 15:08:35 +0530

    :sparkles: Add delete dialog

--- src/components/PaletteList.js
+++ src/components/PaletteList.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import MiniPalette from './MiniPalette';
 import { Link } from 'react-router-dom';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import blue from '@material-ui/core/colors/blue';
+import red from '@material-ui/core/colors/red';
+import CheckIcon from '@material-ui/icons/Check';
+import CloseIcon from '@material-ui/icons/Close';
 import {
-  Root,
   GlobalStyle,
+  Root,
   Container,
   Nav,
   Palettes
@@ -14,12 +25,30 @@ import { CSSTransition } from 'react-transition-group';
 
 class PaletteList extends Component {
 
+  state = {
+    openDeleteDialog: false,
+    deletingId: ''
+  }
+
+  toggleDialog = (id) => {
+    this.setState(st => (
+      { openDeleteDialog: !st.openDeleteDialog, deletingId: id }
+    ));
+  }
+
   goToPalette = (id) => {
     this.props.history.push(`/palette/${id}`);
   }
 
+  handleDelete = () => {
+    const { removePalette } = this.props;
+    const { deletingId } = this.state;
+    this.setState({openDeleteDialog: false}, () => removePalette(deletingId));
+  }
+
   render() {
-    const { palettes, removePalette } = this.props;
+    const { palettes } = this.props;
+    const { openDeleteDialog } = this.state;
     return (
       <Root>
         <GlobalStyle />
@@ -40,11 +69,11 @@ class PaletteList extends Component {
                 <CSSTransition
                   key={palette.id}
                   classNames='fade'
-                  timeout={5000}
+                  timeout={490}
                 >
                   <MiniPalette
                     {...palette}
-                    removePalette={removePalette}
+                    toggleDialog={this.toggleDialog}
                     key={palette.paletteName}
                     goToPalette={this.goToPalette}
                     />
@@ -53,6 +82,41 @@ class PaletteList extends Component {
           </Palettes>
 
         </Container>
+
+        <Dialog
+          open={openDeleteDialog}
+          aria-labelledby='delete-dialog-title'
+          onClose={this.toggleDialog}
+        >
+          <DialogTitle id='delete-dialog-title' >Delete This Palette?</DialogTitle>
+          <List>
+            <ListItem
+              button
+              onClick={this.handleDelete}
+            >
+              <ListItemAvatar>
+                <Avatar
+                  style={{backgroundColor: blue[100], color: blue[600]}}
+                >
+                  <CheckIcon />  
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText>Delete</ListItemText>
+            </ListItem>
+
+            <ListItem button onClick={this.toggleDialog}>
+              <ListItemAvatar>
+                <Avatar
+                  style={{ backgroundColor: red[100], color: red[600]}}
+                >
+                  <CloseIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText>Cancel</ListItemText>
+            </ListItem>
+          </List>
+        </Dialog>
+
       </Root>
     )
   }
